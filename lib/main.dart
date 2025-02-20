48b0ff483fd5320c69eae96ef31a57689ae2b2dd
dev-yamamon <dev.yamamon01@gmail.com>
2025-02-21 01:40:45 +0900

    feat:TabBarとそのcontrolerを作成

--- lib/main.dart
+++ lib/main.dart
@@ -29,35 +29,83 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 5, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: Center(
-        child: Row(
-          children: [
-            // IconButton(onPressed: (){}, icon: Icons.menu,color: Color(0xFFCC0000),),
-            IconButton(
-            onPressed: (){},
-              icon:SvgPicture.asset('assets/menu.svg',width: 30,height: 30),
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Center(
+              child: Row(
+                children: [
+                  // IconButton(onPressed: (){}, icon: Icons.menu,color: Color(0xFFCC0000),),
+                  IconButton(
+                    onPressed: OnMenu,
+                    icon:
+                    SvgPicture.asset('assets/menu.svg', width: 30, height: 30),
+                  ),
+                  Row(children: [
+                    SvgPicture.asset(
+                        'assets/bookmark.svg', width: 30, height: 30),
+                    Text('  '),
+                    Text(
+                      widget.title,
+                      style: TextStyle(color: Color(0xFFCC0000) //AARRGGBB形式のHex
+                      ),
+                    )
+                  ]),
+                  IconButton(
+                      onPressed: OnSecret,
+                      icon:
+                      SvgPicture.asset('assets/key.svg', width: 30, height: 30))
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
               ),
-            Row(children: [
-              SvgPicture.asset('assets/bookmark.svg',width: 30,height: 30),
-              Text('  '),
-              Text(widget.title,
-                style: TextStyle(
-                  color: Color(0xFFCC0000)//AARRGGBB形式のHex
-                ),)
-            ]),
-             IconButton(onPressed: (){}, icon: SvgPicture.asset('assets/key.svg',width: 30,height: 30))
-
-          ],
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        ),
-      )),
-      body: Text("data"),
+            ),
+            bottom: TabBar(
+              controller: _tabController, // カスタムコントローラーを指定
+              tabs: [
+                Tab(icon: Icon(Icons.star), text: "トップ3"),
+                Tab(text: "ジャンル1",),
+                Tab(text: "ジャンル2"),
+                Tab(text: "ジャンル3"),
+                Tab(icon: Icon(Icons.more_horiz)),
+              ],
+            ),
+          ),
+            body: TabBarView(
+                controller: _tabController, // コントローラーを指定
+                children: [
+                  Text("トップ3の内容です"),
+                  Text("ジャンル1の内容です"),
+                  Text("ジャンル2の内容です"),
+                  Text("ジャンル3の内容です"),
+                  Text("#3"),
+                ]),
+          ),
     );
   }
+
+  void OnMenu(){
+    print("menu-button pressed");
+  }
+
+  void OnSecret(){
+    print("secret-button pressed");
+  }
+
 }
