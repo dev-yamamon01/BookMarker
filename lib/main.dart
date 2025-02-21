9e8926155e3a5d32ab2989b53dc7cd0eea9d8232
dev-yamamon <dev.yamamon01@gmail.com>
2025-02-21 23:26:46 +0900

    feat:TabBarのindicatorをカスタマイズ

--- lib/main.dart
+++ lib/main.dart
@@ -78,12 +78,25 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
             ),
             bottom: TabBar(
               controller: _tabController, // カスタムコントローラーを指定
+              indicator: BoxDecoration(
+                color: Colors.grey.withOpacity(0.1), // 背景色（タブの背景全体に適用）
+                border: Border(
+                  bottom: BorderSide(color: Color(0xFFCC0000), width: 4), // 下線のカスタマイズ
+                ),
+              ),
+              indicatorSize: TabBarIndicatorSize.tab, // ラベルのサイズに合わせたインジケーター,
+              labelStyle: TextStyle(
+                  fontSize: 12,
+                fontWeight: FontWeight.bold,
+                  color: Color(0xFFCC0000),
+                  ),
+              unselectedLabelStyle: TextStyle(fontSize: 10),
               tabs: [
-                Tab(icon: Icon(Icons.star), text: "トップ3"),
-                Tab(text: "ジャンル1",),
-                Tab(text: "ジャンル2"),
-                Tab(text: "ジャンル3"),
-                Tab(icon: Icon(Icons.more_horiz)),
+                Tab(icon: Icon(Icons.star), text: "トップ3",height: 60,),
+                Tab(text: 'ジャンル1',height: 40,),
+                Tab(text: "ジャンル2",height: 40,),
+                Tab(text: "ジャンル3",height: 40,),
+                Tab(icon: Icon(Icons.more_horiz),height: 40,),
               ],
             ),
           ),
