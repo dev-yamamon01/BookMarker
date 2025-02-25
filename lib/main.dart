d74d80288b2ea5f1a194bae8af90ea079c5b83fd
dev-yamamon <dev.yamamon01@gmail.com>
2025-02-26 00:07:37 +0900

    feat:Drawerの項目編集

--- lib/main.dart
+++ lib/main.dart
@@ -53,18 +53,49 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
     return MaterialApp(
       home: Scaffold(
         key: _scaffoldKey,
-        drawer: Drawer( // 🔹 `Drawer` の設定
+        drawer: Drawer(
           child: ListView(
             children: [
               DrawerHeader(
-                decoration: BoxDecoration(color: Colors.blue),
-                child: Text("Menu", style: TextStyle(color: Colors.white, fontSize: 24)),
+                decoration: BoxDecoration(
+                    color: Color(0xAAFFFFFF),
+                    borderRadius: BorderRadius.circular(16.0)),
+                child: Column(
+                  children: [
+                    Text('Book Maker',
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Color(0xFFCC0000)
+                      ),
+                    ),
+                  SvgPicture.asset('assets/bookmark.svg', width: 80, height: 80)],
+                )
               ),
               ListTile(
-                leading: Icon(Icons.home),
-                title: Text("Home"),
+                leading: Icon(Icons.account_box),
+                title: Text("アカウント"),
                 onTap: () {},
               ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text("設定"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.menu_book_sharp),
+                title: Text("チュートリアル"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.question_answer_outlined),
+                title: Text("問い合わせ"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.contact_support),
+                title: Text("Book Makerについて"),
+                onTap: () {},
+              )
             ],
           ),
         ),
